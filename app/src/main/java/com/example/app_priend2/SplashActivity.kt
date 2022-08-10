8e57d711c036f4d8b4a556516fe27ab2ac5400bb
김서하 <kimseoha@gimseohaui-MacBookPro.local>
2022-08-10 23:28:06 +0900

    krsthg

---
+++ app/src/main/java/com/example/app_priend2/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.example.app_priend2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        var intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
